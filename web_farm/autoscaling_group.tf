1cd6de80b9666ee064f09c6a1fc54cb8190bad40
Greg Swallow <gswallow@ivytech.edu>
2018-10-01 09:13:56 -0400

    I'm impatient.

--- web_farm/autoscaling_group.tf
+++ web_farm/autoscaling_group.tf
@@ -2,6 +2,7 @@ resource "aws_autoscaling_group" "web" {
   name_prefix          = "${var.ENV}-web-"
   launch_configuration = "${aws_launch_configuration.web.name}"
   health_check_type    = "EC2"
+  health_check_grace_period = 120
   min_size             = 1
   desired_capacity     = 2
   max_size             = 3
