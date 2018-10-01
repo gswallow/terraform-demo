879d74347cc7f33913123391c98687f007870e11
Greg Swallow <gswallow@ivytech.edu>
2018-10-01 07:18:49 -0400

    terraform backends are so brittle.

--- web_farm/_backend.tf
+++ web_farm/_backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-tfstate-216430079837"
+    bucket         = "terraform-tfstate-993356857210"
     key            = "web_farm.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_locks"
+    profile        = "gregonaws"
   }
 }
