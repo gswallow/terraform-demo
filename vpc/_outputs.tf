3fc6d149a75b8e72572a2ec3c827dd846bab6943
Greg Swallow <gswallow@ivytech.edu>
2018-09-30 23:58:25 -0400

    Add outputs

--- vpc/_outputs.tf
+++ vpc/_outputs.tf
@@ -1,7 +1,15 @@
-# output "public_subnet_ids" {
-#   value = "${aws_subnet.public.*.id}"
-# }
+output "vpc_id" {
+  value = "${aws_vpc.current.id}"
+}
 
-# output "private_subnet_ids" {
-#   value = "${aws_subnet.private.*.id}"
-# }
+output "public_subnet_ids" {
+  value = "${aws_subnet.public.*.id}"
+}
+
+output "private_subnet_ids" {
+  value = "${aws_subnet.private.*.id}"
+}
+
+output "default_security_group_id" {
+  value = "${aws_default_security_group.default.id}"
+}
