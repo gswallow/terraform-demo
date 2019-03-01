4ca95af75e1c47ae1bc93ec8d72136d42b813924
Greg Swallow <gswallow@developertown.com>
2019-03-01 00:47:59 -0500

    Update _data.tf

--- web_farm/_data.tf
+++ web_farm/_data.tf
@@ -5,8 +5,8 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 # Here we use the terraform_state_store data source to retrieve the current
-# state of our parent object from S3.  This is an example of using AWS 
-# resources to assist with automation efforts.
+# state of our parent object (the VPC).  This is an example of using terraform's data  
+# sources to assist with automation efforts.
 
 data "terraform_remote_state" "vpc" {
   backend = "local"
